704843d5b75eda76602037a697368b26bfe3cee3
zeroBit <luisivan001@gmail.com>
2020-05-15 21:52:36 -0400

    Initial Commit

---
+++ dnd_test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  dnd_test
+//
+//  Created by lil_box on 5/15/20.
+//  Copyright © 2020 zeroBurnt. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
